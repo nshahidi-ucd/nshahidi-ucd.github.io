3c4bf3b2ee1e533cb4f97b32d8f4e46479e2802f
nshahidi-ucd <143111231+nshahidi-ucd@users.noreply.github.com>
2023-09-10 08:52:39 -0600

    more work on gridloader

--- modules/gridloader.mjs
+++ modules/gridloader.mjs
@@ -1,8 +1,7 @@
-import {def as _def} from './utilMod.mjs';
+import {def as _def, ref as _ref} from './utilMod.mjs';
 
 (function(){
     var tho = {},
-    def = function(a,b,c){_def(a,b,c,tho)};
-    def(['doc','test1','test2'],[window.document,3,function(){console.log(this.test1+7)}],'001');
-    tho.test2();
+    [def,ref]=[function(a,b,c){_def(a,b,c,tho)},function(a,b,c){_ref(a,b,tho,c)}];
+    def(['doc','makel','setattrs','setcinf','addel'],[window.document,function(a){return(ref(['doc','createElement'],[0,[a]]));},function(a,b){for(var c in a)_ref('setAttribute',[c,a[c]],b,true);},function(){var[e,f,g]=[{},arguments[0],_ref('slice',[1],[...arguments])];_def(['class','id','name','data-for'],g,'0000',e);ref('seattrs',[f,e],true);},function(a,b){var[c]=b?b:ref(['doc','body'],[0,0]);_ref('appendChild',[a],true);}],'01011');var test1=ref('makel',['span']);ref('setcinf',[test1,'test-class','test_id','testname','testHost'],1);ref('addel',[test1]);
 })();
--- modules/utilMod.mjs
+++ modules/utilMod.mjs
@@ -1,1 +1,1 @@
-export function def(ma,ka,ha,wa){for(var[na]=[0];na<ma.length;++na)wa[ma[na]]=!eval(ha[na])?ka[na]:ka[na]['bind'](wa)}
+export function def(ma,ka,ha,wa){var[sa,ta]=Array.isArray(ma)?[ma,ka]:[[ma],[ka]];for(var[na]=[0];na<sa.length;++na)wa[sa[na]]=!eval(ha[na])?ta[na]:ta[na]['bind'](wa)}export function ref(ba,da,fa,pa){var ja=fa,qa=window.btoa(nul),la=!window.Array.isArray(ba)?da?ja[ba](...da):ja[ba]:qa;if(la!=qa)return(pa?undefined:la);for(var[ga]=[0];ga<ba.length;++ga)ja=da[ga]?ja[ba[ga]](...da[ga]):ja[ba[ga]];if(!pa)return(ja)}
