88b5b2e1fc8d4b30d86faf84826247e702b0b8e5
nshahidi-ucd <143111231+nshahidi-ucd@users.noreply.github.com>
2023-09-10 06:06:05 -0600

    First test of gridloader

---
+++ gridloader.mjs
@@ -0,0 +1,8 @@
+import {def as _def} from 'utilMod.mjs';
+
+(function(){
+    var tho = {},
+    def = function(a,b,c){_def(a,b,c,tho)};
+    def(['doc','test1','test2'],[window.document,3,function(){console.log(this.test1+7)}],'001');
+    tho.test2();
+})();
